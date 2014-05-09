c774dc499e78704d706750c5b94e046e45617467
jonschlinkert <dev@sellside.com>
2014-05-08 20:10:32 -0400

    stringify tests

--- test/stringify.js
+++ test/stringify.js
@@ -30,3 +30,26 @@ describe('rte.stringify()', function() {
     expect(actual).to.eql('foo/foo/bar/baz/index.html');
   });
 });
+
+describe('rte.stringify()', function() {
+  rte.set('one', ':dirname/:basename:ext');
+  rte.set('two', ':dirname/foo/bar/baz/:basename:ext');
+
+  it('should return a string using the named structure', function() {
+    var actual = rte.stringify('one');
+    expect(actual).to.eql('foo/index.html', {
+      dirname: 'foo',
+      basename: 'index',
+      ext: '.html'
+    });
+  });
+
+  it('should return a string using the named structure', function() {
+    var actual = rte.stringify('two');
+    expect(actual).to.eql('foo/foo/bar/baz/index.html', {
+      dirname: 'foo',
+      basename: 'index',
+      ext: '.html'
+    });
+  });
+});
