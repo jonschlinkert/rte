b0aa6df6f4b18e2ddf5174cb5deabf6b104dd6db
jonschlinkert <dev@sellside.com>
2014-05-08 20:11:56 -0400

    stringify

--- test/stringify.js
+++ test/stringify.js
@@ -32,24 +32,18 @@ describe('rte.stringify()', function() {
 });
 
 describe('rte.stringify()', function() {
-  rte.set('one', ':dirname/:basename:ext');
-  rte.set('two', ':dirname/foo/bar/baz/:basename:ext');
+  var route = new Route();
+
+  route.set('one', ':dirname/:basename:ext');
+  route.set('two', ':dirname/foo/bar/baz/:basename:ext');
 
   it('should return a string using the named structure', function() {
-    var actual = rte.stringify('one');
-    expect(actual).to.eql('foo/index.html', {
-      dirname: 'foo',
-      basename: 'index',
-      ext: '.html'
-    });
+    var actual = route.stringify('one');
+    expect(actual).to.eql('foo/index.html');
   });
 
   it('should return a string using the named structure', function() {
-    var actual = rte.stringify('two');
-    expect(actual).to.eql('foo/foo/bar/baz/index.html', {
-      dirname: 'foo',
-      basename: 'index',
-      ext: '.html'
-    });
+    var actual = route.stringify('two');
+    expect(actual).to.eql('foo/foo/bar/baz/index.html');
   });
 });
