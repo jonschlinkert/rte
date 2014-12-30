f2db81169375a9dfa8d103bc492fcd8bb2d3abf9
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-30 06:55:38 -0500

    build readme

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * rte <https://github.com/jonschlinkert/rte>
  *
- * Copyright (c) 2014, Jon Schlinkert, Brian Woodward
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
